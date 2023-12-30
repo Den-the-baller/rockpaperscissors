1be19be018d461ab39b33cecc98b7738f2c2d4fd
Dennis Ballerstein <dennisballerstein@Denniss-MacBook-Pro.local>
2023-12-30 11:47:16 -0700

    dded human choic

--- rps.js
+++ rps.js
@@ -12,6 +12,29 @@ function getComputerChoice(){
     return compPick
 
 }
+
+correctInput = 0
+
+console.log("Welcome to your favorite game - Rock, Paper, Scissors. You will be playing against the computer. Good luck!")
+
+
+while (correctInput == 0){
+    humanChoice = prompt("Please choose Rock, Paper or Scissors")
+
+    if (humanChoice == "Rock"){
+        correctInput = 1
+    } else if (humanChoice == "Scissors"){
+        correctInput = 1
+    } else if (humanChoice == "Paper"){
+        correctInput = 1
+    } else {
+        console.log("Invalid input.")
+    }
+}
+
+
+
+
 compChoice = ""
 compChoice = getComputerChoice()
 console.log(compChoice)
