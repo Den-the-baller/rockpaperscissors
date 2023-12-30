4b353b7493a998638b2886f6871ab3f7a546ea7b
Dennis Ballerstein <dennisballerstein@Denniss-MacBook-Pro.local>
2023-12-30 16:44:21 -0700

    Added functionality to validate human input and same pick validation  to determine a winner based off chaoices

--- rps.js
+++ rps.js
@@ -13,30 +13,70 @@ function getComputerChoice(){
 
 }
 
-correctInput = 0
+// correctInput = 0
+// console.log("Welcome to your favorite game - Rock, Paper, Scissors. You will be playing against the computer. Good luck!")
 
+// while (correctInput == 0){
+//     humanChoice = prompt("Please choose Rock, Paper or Scissors")
+
+//     if (humanChoice == "Rock"){
+//         correctInput = 1
+//     } else if (humanChoice == "Scissors"){
+//         correctInput = 1
+//     } else if (humanChoice == "Paper"){
+//         correctInput = 1
+//     } else {
+//         console.log("Invalid input.")
+//     }
+//     console.log("You picked " +humanChoice+"!" )
+// }
+
+
+// compChoice = ""
+// compChoice = getComputerChoice()
+// console.log("The computer picked "+compChoice+"!")
+sameChoice = 0 
+compChoice = ""
 console.log("Welcome to your favorite game - Rock, Paper, Scissors. You will be playing against the computer. Good luck!")
 
-
-while (correctInput == 0){
-    humanChoice = prompt("Please choose Rock, Paper or Scissors")
-
-    if (humanChoice == "Rock"){
-        correctInput = 1
-    } else if (humanChoice == "Scissors"){
-        correctInput = 1
-    } else if (humanChoice == "Paper"){
-        correctInput = 1
-    } else {
-        console.log("Invalid input.")
+while (sameChoice == 0){
+    correctInput = 0
+    while (correctInput == 0){
+        humanChoice = prompt("Please choose Rock, Paper or Scissors")
+        if (humanChoice == "Rock"){
+            correctInput = 1
+        } else if (humanChoice == "Scissors"){
+            correctInput = 1
+        } else if (humanChoice == "Paper"){
+            correctInput = 1
+        } else {
+            console.log("Invalid input.")
+        }
     }
     console.log("You picked " +humanChoice+"!" )
+
+    compChoice = getComputerChoice()
+    console.log("The computer picked "+compChoice+"!")
+
+
+    if (humanChoice == compChoice) {
+        console.log("You both picked the same thing. Try again!")
+
+    }else if (compChoice == "Rock" && humanChoice == "Scissors"){
+        sameChoice = 1
+        console.log("Computer wins, you lose!")
+    } else if (compChoice == "Paper" && humanChoice == "Rock"){
+        sameChoice = 1
+        console.log("Computer wins, you lose!")
+    } else if (compChoice == "Scissors" && humanChoice == "Paper"){
+        sameChoice = 1
+        console.log("Computer wins, you lose!")
+    } else {
+        sameChoice = 1
+        console.log("Nice job, you win!")
+    }
+
 }
+console.log("That is the end of the gane")
 
 
-
-
-compChoice = ""
-compChoice = getComputerChoice()
-console.log("The computer picked "+compChoice+"!")
-console.log("EOF")
